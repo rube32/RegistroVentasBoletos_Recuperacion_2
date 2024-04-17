95d0f72a1db7eee50fd8156ae684381fd2aa5d68
Chuchullo_Flores_John_Ruben <201123187j@utea.edu.pe>
2024-04-17 09:43:53 -0500

    Se crea la clase Ruta al proyecto

---
+++ RegistroVentasBoletos_Recuperacion_2/Ruta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroVentasBoletos_Recuperacion_2
+{
+    class Ruta
+    {
+        
+    public string Nombre { get; set; }
+        public decimal PrecioBase { get; set; }
+    }
+}
