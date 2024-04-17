fea15ffed2ccb724800c4d6643a4cc471ea7038e
Chuchullo_Flores_John_Ruben <201123187j@utea.edu.pe>
2024-04-17 09:40:49 -0500

    Se crea la clase Cliente al proyecto

---
+++ RegistroVentasBoletos_Recuperacion_2/Cliente .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroVentasBoletos_Recuperacion
+{
+    class Cliente
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public bool EsPromocionColegios { get; set; }
+        public Cliente(string nombre, int edad, bool esPromocionColegios)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            EsPromocionColegios = esPromocionColegios;
+        }
+
+    }
+}
